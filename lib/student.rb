78c843985816001f687b430562716c1a26358851
William West <wwest466@gmail.com>
2020-10-24 14:38:33 +0000

    Automatically backed up by Learn

--- lib/student.rb
+++ lib/student.rb
@@ -13,7 +13,7 @@ attr_reader :id
       CREATE TABLE IF NOT EXISTS students (
         id INTEGER PRIMARY KEY,
         name TEXT,
-        album TEXT
+        grade TEXT
       )
       SQL
     DB[:conn].execute(sql)
