7b173c8a9e83f29a813690755bb575ee5a116670
William West <wwest466@gmail.com>
2020-10-24 14:38:06 +0000

    Automatically backed up by Learn

--- lib/student.rb
+++ lib/student.rb
@@ -26,6 +26,14 @@ attr_reader :id
     DB[:conn].execute(sql)
   end
 
+  def save
+    sql = <<-SQL
+      INSERT INTO students (name, grade)
+      VALUES (?,?)
+    SQL
+    DB[:conn].execute(sql, self.name, self.grade)
+  end
+  
 end
 # Remember, you can access your database connection anywhere in this class
 #  with DB[:conn]
