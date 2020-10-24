af67a99e64f2b1e9e8686818cd9ae2ba308765d2
William West <wwest466@gmail.com>
2020-10-24 14:32:33 +0000

    Automatically backed up by Learn

--- lib/student.rb
+++ lib/student.rb
@@ -20,12 +20,12 @@ attr_reader :id
   end
 
   def self.drop_table
-    sql = <<-SQL 
-      DROP TABLE IF EXISTS students 
+    sql = <<-SQL
+      DROP TABLE IF EXISTS students
       SQL
     DB[:conn].execute(sql)
   end
-  
-end  
+
+end
 # Remember, you can access your database connection anywhere in this class
 #  with DB[:conn]
